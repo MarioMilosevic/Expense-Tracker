d385bb50b250952a32b323fe69bb180d086d658e
Mario Milošević <mariomilosevic887@gmail.com>
2024-01-07 09:29:24 +0100

    Select and add constants from constants js into main js

---
+++ constants.js
@@ -0,0 +1,25 @@
+"use strict";
+
+export function constants() {
+  const budgetInput = document.querySelector("#budget");
+  const setBudgetBtn = document.querySelector("#setBtn");
+  const expenseNameInput = document.querySelector("#expenseName");
+  const expenseCostInput = document.querySelector("#expenseCost");
+  const addTransactionBtn = document.querySelector(".addBtn");
+  const totalBudget = document.querySelector("#totalBudget");
+  const totalExpenses = document.querySelector("#totalExpenses");
+  const totalBalance = document.querySelector("#totalBalance");
+  const transactionHistory = document.querySelector(".transactionHistory");
+
+  return {
+    budgetInput,
+    setBudgetBtn,
+    expenseNameInput,
+    expenseCostInput,
+    addTransactionBtn,
+    totalBudget,
+    totalExpenses,
+    totalBalance,
+    transactionHistory,
+  };
+}
--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 'use strict'
+import { constants } from "./constants"
+
+const {
+    budgetInput,
+    setBudgetBtn,
+    expenseNameInput,
+    expenseCostInput,
+    addTransactionBtn,
+    totalBudget,
+    totalExpenses,
+    totalBalance,
+    transactionHistory,
+} = constants()
+
