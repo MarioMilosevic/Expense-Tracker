1b090cc2af7febe98154ea4fae644dc0258597e6
Mario Milošević <mariomilosevic887@gmail.com>
2024-01-09 17:07:05 +0100

    Fix el removal on remove button click from array bug

--- helpers.js
+++ helpers.js
@@ -25,3 +25,16 @@ export const getCurrentDate = () => {
   } ${year}`;
   return text;
 };
+
+export const createDiv = (obj, index) => {
+  const transactionDiv = document.createElement("div");
+  transactionDiv.classList.add("transactionDescription");
+  transactionDiv.innerHTML = `
+      <div>${index + 1}.</div>
+      <div>${obj.name}</div>
+      <div>$${obj.cost}</div>
+      <div>${obj.date}</div>
+      <div>${obj.actions}</div>
+      <button class="removeBtn">X</button>
+    `;
+}
--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 "use strict";
 import { constants } from "./constants";
 import { Budget, TransactionManager, Transaction, UI } from "./classes";
-import { getCurrentDate } from "./helpers";
+import { getCurrentDate, createDiv } from "./helpers";
 // import Toastify from 'toastify-js'
 // import "toastify-js/src/toastify.css"
-
-
 
 const {
   budgetInput,
@@ -46,7 +44,7 @@ addTransactionBtn.addEventListener("click", function () {
     if (expenseCostValue > 0) {
       budget.setExpense(expenseCostValue);
       if (budget.getBalance() >= budget.getExpense()) {
-        budget.addExpense(budget.getExpense())
+        budget.addExpense(budget.getExpense());
         totalExpenses.textContent = `$${budget.totalExpenses()}`;
         const date = getCurrentDate();
         counter++;
@@ -59,9 +57,9 @@ addTransactionBtn.addEventListener("click", function () {
         );
 
         transactionMan.add(transaction);
-        createTransaction2(transactionHistory, transaction);
+        createTransaction2(transactionHistory);
 
-        console.log(transactionMan.getTransactions());
+        // console.log(transactionMan.getTransactions());
         budget.calculateBalance();
         totalBalance.textContent = `$${budget.getBalance()}`;
         userInterface.show(mainTransactionDescription);
@@ -79,30 +77,63 @@ addTransactionBtn.addEventListener("click", function () {
   }
 });
 
-const createTransaction = (parent, transaction) => {
-  const transactionDiv = document.createElement("div");
-  transactionDiv.classList.add("transactionDescription");
-  transactionDiv.innerHTML = "";
-  transactionDiv.innerHTML = `
-        <div>${transaction.num}.</div>
-        <div>${transaction.name}</div>
-        <div>$${transaction.cost}</div>
-        <div>${transaction.date}</div>
-        <div>${transaction.actions}</div>
-        <button class="removeBtn">X</button>
-    `;
-  parent.appendChild(transactionDiv);
 
-  const removeBtn = transactionDiv.querySelector(".removeBtn");
-  removeBtn.addEventListener("click", function () {
-    transactionMan.remove(transaction.id);
-    removeBtn.parentElement.remove();
-    if (transactionMan.getTransactions().length === 0) {
-      userInterface.show(transactionHistoryHeading);
-      userInterface.hide(mainTransactionDescription);
+const createTransaction2 = (parent) => {
+  console.log("array prije", transactionMan.getTransactions());
+  parent.innerHTML = "";
+  transactionMan.getTransactions().forEach((el, i) => {
+    //  createDiv(el, i)
+    console.log(el);
+    const transactionDiv = document.createElement("div");
+    transactionDiv.classList.add("transactionDescription");
+    transactionDiv.innerHTML = `
+        <div>${i + 1}.</div>
+        <div>${el.name}</div>
+        <div>$${el.cost}</div>
+        <div>${el.date}</div>
+        <div>${el.actions}</div>
+        <button class="removeBtn">X</button>
+      `;
+
+    const removeBtn = transactionDiv.querySelector(".removeBtn");
+    removeBtn.addEventListener("click", function () {
+// ///////////////////////////////////////////
+      transactionMan.remove(el.id);
+      console.log("array posle", transactionMan.getTransactions());
+    });
+
+    /* removeBtn.addEventListener("click", function () {
+
+      parent.innerHTML = ''
+      transactionMan.getTransactions().forEach((el, i) => {
+      const transactionDiv = document.createElement("div");
+    transactionDiv.classList.add("transactionDescription");
+    transactionDiv.innerHTML = `
+        <div>${i + 1}.</div>
+        <div>${el.name}</div>
+        <div>$${el.cost}</div>
+        <div>${el.date}</div>
+        <div>${el.actions}</div>
+        <button class="removeBtn">X</button>
+      `;
+      // createDiv(el,i)
+      parent.appendChild(transactionDiv);
+    })
+
+      // removeBtn.parentElement.remove();
+      if (transactionMan.getTransactions().length === 0) {
+        userInterface.show(transactionHistoryHeading);
+        userInterface.hide(mainTransactionDescription);
+      }
     }
+    );*/
+    parent.appendChild(transactionDiv);
   });
+  // iznad se petlja zavrsava
 };
+
+// sve da ispzanim pa sve napravim sa stanjem u arrayu
+// kada brisem isto uklonim, i napravim sve nakon praznjenja
 
 
 
@@ -121,24 +152,3 @@ const createTransaction = (parent, transaction) => {
 //     <button class="removeBtn">X</button>
 //   })`;
 //   parent.appendChild(transactionDiv))
-
-  const createTransaction2 = (parent, transaction) => {
-    parent.innerHTML = ''
-    transactionMan.getTransactions().forEach((el, index) => {
-      const transactionDiv = document.createElement("div");
-      transactionDiv.classList.add("transactionDescription");
-      transactionDiv.innerHTML = `
-        <div>${index + 1}.</div>
-        <div>${el.name}</div>
-        <div>$${el.cost}</div>
-        <div>${el.date}</div>
-        <div>${el.actions}</div>
-        <button class="removeBtn">X</button>
-      `;
-      parent.appendChild(transactionDiv);
-      
-    });
-  };
-  
-  // sve da ispzanim pa sve napravim sa stanjem u arrayu
-  // kada brisem isto uklonim, i napravim sve nakon praznjenja 
